415ffa1983131bcd2f807769f6d9e26e152bb12b
Jennie T <jenniet.@Jennies-MacBook-Pro.local>
2025-07-21 21:36:03 +0800

    feat: update Z value

--- monte_carlo_app.py
+++ monte_carlo_app.py
@@ -125,7 +125,7 @@ def monte_carlo_simulation(ticker='SMCI'):
                     status_text.text(f"Simulation progress: {int(progress * 100)}%")
 
                     # Generate array of random standard normal values
-                    Z = np.random.standard_normal(days)
+                    Z = np.random.normal(0,1,size=(days,num_simulations))
 
                     # Initialize price array
                     price_path = np.zeros(days)
